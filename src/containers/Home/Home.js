d1b2e35be847aaa14dab2a8322744f7c271a1b2c
iNikNik <game.nik.nik@gmail.com>
2016-02-01 16:49:25 +0300

    Adding 3000+ points

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -12,6 +12,10 @@ class Home extends Component {
     ticksData: PropTypes.array.isRequired,
   };
 
+  stopWS() {
+    global.socket.close();
+  }
+
   render() {
     const styles = require('./Home.scss');
     const { ticksData } = this.props;
@@ -21,9 +25,12 @@ class Home extends Component {
         <Helmet title="Home"/>
         <div className={styles.masthead}>
           <div className="container">
-            <h1>EURUSD Currency</h1>
+            <h1>EURUSD Currency [{ticksData.length} points]</h1>
           </div>
           { !ticksData.length && <p>loading initial data ...</p> }
+        </div>
+        <div className="row text-center">
+          <button className={`btn btn-danger ${styles.bufferBottom}`} type="button" onClick={this.stopWS}>Stop websocket</button>
         </div>
         <div>
           { ticksData.length && <PriceTickChart data={ticksData} type="hybrid" />}
--- src/helpers/priceData/priceDataServerSocket.js
+++ src/helpers/priceData/priceDataServerSocket.js
@@ -4,6 +4,8 @@ import priceData from './priceData';
 import { INITIAL_DATA, NEW_TICK } from './MessageTypes';
 
 
+const finalPriceData = [ ...priceData, ...[...priceData].reverse(), ...priceData ];
+
 function createMessage(type, body) {
   return JSON.stringify({ type, body });
 }
@@ -25,18 +27,20 @@ export function ticksSource() {
   wss.on('connection', function connection(ws) {
     let counter = 0;
     const interval = setInterval(() => {
-      if (counter + 200 >= priceData.length) {
+      if (counter + 3000 >= finalPriceData.length) {
         ws.close();
       }
-      ws.send(createMessage(NEW_TICK, fillDate(priceData[200 + counter])));
+      ws.send(createMessage(NEW_TICK, fillDate(finalPriceData[3000 + counter])));
       counter++;
     }, 350);
+
+    // console.log('finalPriceData.length = ', finalPriceData.length);
 
     ws.on('message', function incoming(message) {
       console.log('received: %s', message);
     });
 
-    ws.send(createMessage(INITIAL_DATA, getInitialData(priceData, 200)));
+    ws.send(createMessage(INITIAL_DATA, getInitialData(finalPriceData, 3000)));
     ws.on('close', () => clearInterval(interval));
     ws.on('error', () => clearInterval(interval));
   });
